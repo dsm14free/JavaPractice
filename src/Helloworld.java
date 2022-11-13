a3355912509af61a9e553cd7c4e123180fb79e8f
dsm14free <dsm18free@gmail.com>
2022-11-12 21:02:02 -0800

    Adding java class files.

---
+++ src/Helloworld.java
@@ -0,0 +1,10 @@
+public class Helloworld {
+    public static void main(String args[]){
+        Helloworld hello = new Helloworld();
+        hello.newMethod();
+    }
+
+    public void newMethod() {
+        System.out.println("This is HelloWorld");
+    }
+}
