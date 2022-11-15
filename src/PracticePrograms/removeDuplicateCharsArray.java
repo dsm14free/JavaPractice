c6efebfc12ed167e9054b148da08571642e3f370
dsm14free <dsm18free@gmail.com>
2022-11-14 21:10:08 -0800

    Committing new java files

---
+++ src/PracticePrograms/removeDuplicateCharsArray.java
@@ -0,0 +1,25 @@
+package PracticePrograms;
+
+import java.util.Scanner;
+
+public class removeDuplicateCharsArray {
+	public static void removeDuplicates() {
+		try (Scanner input = new Scanner(System.in)){
+			System.out.println("Enter the String:");
+			String str = input.next();
+			char[] charArray = str.toCharArray();
+			String newString = "";
+			
+			for (char value: charArray) {
+				if (newString.indexOf(value)==-1) {
+					newString+=value;
+				}
+			}
+			System.out.println("The new String is: "+newString);
+		}
+	}
+	
+	public static void main(String[] args) {
+		removeDuplicateCharsArray.removeDuplicates();
+	}
+}
