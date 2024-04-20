41d753366ba903af64e15910670f94205276b2a8
dsm14free <dsm18free@gmail.com>
2024-04-20 14:47:55 -0500

    New program for Thread

--- src/PracticePrograms/ThreadContructorRunStr.java
+++ src/PracticePrograms/ThreadContructorRunStr.java
@@ -5,10 +5,10 @@ public class ThreadContructorRunStr implements Runnable {
     public void run(){
         System.out.println("Now the thread is running");
     }
-    
+
     public static void main(String[] args){
         Runnable r1 = new ThreadContructorRunStr();
-        Thread th1 = new Thread(r1, "New thread");
+        Thread th1 = new Thread(r1, "New thread created");
         th1.start();
         String name = th1.getName();
         System.out.println("Name of the thread is: "+name);
